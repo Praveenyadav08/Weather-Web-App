ef66310e3b004e34119d8d98a7eee643bdc4ddd8
Praveenyadav08 <praveenyadaav8@gmail.com>
2025-08-28 10:38:34 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,48 @@
+let data;
+
+
+// variables to store the data->
+
+const inputBox = document.getElementById("inputBox");
+const CountryName =document.getElementById("countryName");
+const stateName= document.getElementById("stateName");
+const cityName=document.getElementById("cityName");
+const humidity = document.getElementById("humidity");
+const  windSpeed =document.getElementById("windSpeed");
+const temperature =document.getElementById("temperature");
+const logoImage =document.getElementById("logoImage");
+const weatherStatus= document.getElementById("weatherStatus")
+
+
+const getData =  async (event) => {
+    event.preventDefault();
+    if(!inputBox.value){
+        alert("Please Enter the City Name : ")
+        return ;
+    }
+
+
+const city=inputBox.value;
+
+const fetchData =  await fetch(`http://api.weatherapi.com/v1/current.json?key=80fddd08545d482685580715252206&q=${city}`);
+
+const orgData = await fetchData.json();
+data = orgData;
+console.log(data);
+
+
+CountryName.innerText= data.location.country;
+stateName.innerText =data.location.region;
+cityName.innerText=data.location.name;
+humidity.innerText=data.current.humidity;
+windSpeed.innerText=data.current.wind_kph;
+temperature.innerText=data.current.temp_c;
+logoImage.src= data.current.condition.icon;
+weatherStatus.innerText =data.current.condition.text;
+
+
+
+
+};
+
+
